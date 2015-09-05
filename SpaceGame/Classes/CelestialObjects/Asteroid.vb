e9cf2d1fc0106b8ca7ef8d0976ceff92bea85d84
Pete Jespers <petejespers@me.com>
2015-09-05 20:19:09 +0100

    Added asteroid, moon and planet

---
+++ SpaceGame/Classes/CelestialObjects/Asteroid.vb
@@ -0,0 +1,16 @@
+﻿Imports SpaceGame.Classes.BaseEntities
+Imports SpaceGame.Classes.Messaging
+
+Namespace Classes.CelestialObjects
+    Public Class Asteroids
+        Inherits BaseCelestialObject
+
+        Public Overrides Sub Update()
+            Throw New NotImplementedException()
+        End Sub
+
+        Public Overrides Function HandleMessage(msg As Telegram) As Boolean
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
