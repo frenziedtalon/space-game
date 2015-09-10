0922775b115f805522ce8ac7d50ae1a82f98e1f9
Pete Jespers <petejespers@me.com>
2015-09-10 07:05:14 +0100

    Added IState interface

---
+++ SpaceGame/Classes/BaseEntities/IState.vb
@@ -0,0 +1,28 @@
+﻿Imports SpaceGame.Classes.Messaging
+
+Namespace Classes.BaseEntities
+
+    Public Interface IState(Of T)
+
+        ''' <summary>
+        ''' This will execute when the state is entered
+        ''' </summary>
+        Sub State_Enter(ByRef p As T)
+
+        ''' <summary>
+        ''' This is the state's normal update function
+        ''' </summary>
+        Sub State_Execute(ByRef p As T)
+
+        ''' <summary>
+        ''' This will execute when the state is exited
+        ''' </summary>
+        Sub State_Exit(ByRef p As T)
+
+        ''' <summary>
+        ''' This executes if the agent receives a message from the message dispatcher
+        ''' </summary>
+        Function OnMessage(ByRef p As T, msg As Telegram) As Boolean
+
+    End Interface
+End Namespace
