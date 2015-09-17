7cdb587fb110cf0208db91c2f6e0d96a4a698304
Pete Jespers <petejespers@me.com>
2015-09-17 19:49:17 +0100

    Added a viewport and some empty handlers

--- DemoForm/Form1.Designer.vb
+++ DemoForm/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class MainForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,72 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.pnlViewPort = New System.Windows.Forms.Panel()
+        Me.panel2 = New System.Windows.Forms.Panel()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.pnlViewPort.SuspendLayout()
+        Me.panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlViewPort
+        '
+        Me.pnlViewPort.Controls.Add(Me.panel2)
+        Me.pnlViewPort.Location = New System.Drawing.Point(12, 12)
+        Me.pnlViewPort.Name = "pnlViewPort"
+        Me.pnlViewPort.Size = New System.Drawing.Size(984, 708)
+        Me.pnlViewPort.TabIndex = 0
+        '
+        'panel2
+        '
+        Me.panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.panel2.Controls.Add(Me.label2)
+        Me.panel2.Controls.Add(Me.label1)
+        Me.panel2.Location = New System.Drawing.Point(669, 13)
+        Me.panel2.Name = "panel2"
+        Me.panel2.Size = New System.Drawing.Size(302, 56)
+        Me.panel2.TabIndex = 12
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label2.Location = New System.Drawing.Point(12, 32)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(151, 16)
+        Me.label2.TabIndex = 1
+        Me.label2.Text = "Press Escape to Exit"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.Location = New System.Drawing.Point(12, 9)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(183, 16)
+        Me.label1.TabIndex = 0
+        Me.label1.Text = "Press O key to show orbit"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(1008, 732)
+        Me.Controls.Add(Me.pnlViewPort)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Demo"
+        Me.pnlViewPort.ResumeLayout(False)
+        Me.panel2.ResumeLayout(False)
+        Me.panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Private WithEvents pnlViewPort As Panel
+    Private WithEvents panel2 As Panel
+    Private WithEvents label2 As Label
+    Private WithEvents label1 As Label
 End Class
--- DemoForm/Form1.vb
+++ DemoForm/Form1.vb
@@ -1,3 +1,17 @@
-﻿Public Class Form1
+﻿Public Class MainForm
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub MainForm_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+
+    End Sub
+
+    Private Sub pnlViewPort_MouseDown(sender As Object, e As MouseEventArgs) Handles pnlViewPort.MouseDown
+
+    End Sub
+
+    Private Sub pnlViewPort_MouseUp(sender As Object, e As MouseEventArgs) Handles pnlViewPort.MouseUp
+
+    End Sub
 End Class
