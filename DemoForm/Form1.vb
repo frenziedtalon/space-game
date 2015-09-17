4e4fcfae7592441eb6c51a99843090c81217747e
Pete Jespers <petejespers@me.com>
2015-09-17 21:15:05 +0100

    Added initialise method with a single star

--- DemoForm/Form1.vb
+++ DemoForm/Form1.vb
@@ -1,6 +1,9 @@
-﻿Public Class MainForm
-    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports System.Windows.Media.Media3D
+Imports OrbitalMechanics.CelestialObjects
 
+Public Class MainForm
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Initialise()
     End Sub
 
     Private Sub MainForm_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
@@ -12,6 +15,21 @@
     End Sub
 
     Private Sub pnlViewPort_MouseUp(sender As Object, e As MouseEventArgs) Handles pnlViewPort.MouseUp
+
+    End Sub
+
+
+    Private SolarSystem As List(Of ICelestialObject)
+    Private Sub Initialise()
+
+        ' create all the objects with their positions and speeds
+        SolarSystem = New List(Of ICelestialObject)
+
+        Dim sol As New Star("Sol", 100000, 5000, "sol.jpg", New Point3D(0, 0, 0), New Vector3D(0, 0, 0), 100)
+
+        SolarSystem.Add(sol)
+
+
 
     End Sub
 End Class
