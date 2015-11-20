45feb7a2074c6d8f5ecf8e8f2897c9cb10f5262d
Pete Jespers <petejespers@me.com>
2015-11-20 17:34:12 +0000

    Added sceneApiController

---
+++ SpaceGame.Tests/Controllers/SceneApiControllerTest.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports SpaceGame.Controllers
+
+Namespace Controllers
+
+    <TestClass()> Public Class SceneApiControllerTest
+
+        <TestMethod()> Public Sub GetSceneObjects()
+
+            'Arrange
+            Dim controller As New SceneApiController()
+
+            'Act
+            Dim result As IEnumerable(Of String) = controller.GetSceneObjects()
+
+            'Assert
+            Assert.IsNotNull(result)
+            Assert.AreNotEqual(0, result.Count())
+
+        End Sub
+
+    End Class
+End Namespace
---
+++ SpaceGame/Controllers/SceneApiController.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Net
+Imports System.Web.Http
+
+Namespace Controllers
+    Public Class SceneApiController
+        Inherits ApiController
+
+        ' GET api/values
+        Public Function GetSceneObjects() As IEnumerable(Of String)
+            Return New String() {"value1", "value2"}
+        End Function
+
+    End Class
+End Namespace
