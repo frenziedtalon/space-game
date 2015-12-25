6b4a80616e0e1a66751228bd9253f48639226a73
Pete Jespers <petejespers@me.com>
2015-12-25 21:13:54 +0000

    SG-63 Added new project to hold the web api controllers

---
+++ WebApi/App_Start/RouteConfig.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Mvc
+Imports System.Web.Routing
+
+Public Module RouteConfig
+    Public Sub RegisterRoutes(ByVal routes As RouteCollection)
+        routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
+
+    End Sub
+End Module
---
+++ WebApi/Controllers/SceneController.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Net
+Imports System.Web.Http
+Imports OrbitalMechanics
+Imports WebApi.Models
+
+Namespace Controllers
+    Public Class SceneController
+        Inherits ApiController
+
+        ' GET api/values
+        Public Function GetSceneObjects() As SolarSystem
+            Dim constructor = New SceneConstructor
+            Return constructor.SolSystem()
+        End Function
+
+    End Class
+End Namespace
