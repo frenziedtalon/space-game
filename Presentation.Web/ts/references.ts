e26109f62ad207e053a724b97adcecea3618ce2c
Pete Jespers <petejespers@me.com>
2016-01-01 08:36:41 +0000

    Revert "SG-62 Added definition file lib.es6.d.ts"
    This reverts commit c6fda43918c851c75176c33895ad028633877cc3.

--- Presentation.Web/ts/references.ts
+++ Presentation.Web/ts/references.ts
@@ -1,4 +1,3 @@
-﻿/// <reference path="d.ts/lib.d.ts"/>
-/// <reference path="d.ts/lib.es6.d.ts"/>
-/// <reference path="d.ts/jquery.d.ts" />
+﻿/// <reference path="d.ts/jquery.d.ts" />
 /// <reference path="d.ts/babylon.d.ts" />
+/// <reference path="d.ts/lib.d.ts"/>
