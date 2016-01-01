6f1f9acbe4e346989e1eca42d4c640786a2a33de
Pete Jespers <petejespers@me.com>
2016-01-01 07:57:00 +0000

    SG-62 Fixed incorrect call of drawCircle

--- Presentation.Web/ts/app.ts
+++ Presentation.Web/ts/app.ts
@@ -185,7 +185,7 @@ var createScene = () => {
         planet.isPickable = planetInfo.CameraTarget;
 
         // draw planet's orbit
-        drawCircle(planetInfo.Orbit.Radius, planetInfo.Name + "Orbit");
+        drawCircle(planetInfo.Orbit.Radius, planetInfo.Name + "Orbit", null);
 
         // create any moons
         if (planetInfo.hasOwnProperty("Moons")) {
@@ -255,7 +255,7 @@ var createScene = () => {
         }
     }
 
-    function drawCircle(radius, meshName, parent) {
+    function drawCircle(radius, meshName, parent: BABYLON.Mesh) {
 
         var tes = radius / 2; // number of path points, more is smoother
         if (tes < 40) {
