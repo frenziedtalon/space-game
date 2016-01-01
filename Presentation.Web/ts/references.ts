c6fda43918c851c75176c33895ad028633877cc3
Pete Jespers <petejespers@me.com>
2016-01-01 08:28:57 +0000

    SG-62 Added definition file lib.es6.d.ts

--- Presentation.Web/ts/references.ts
+++ Presentation.Web/ts/references.ts
@@ -1,3 +1,4 @@
-﻿/// <reference path="d.ts/jquery.d.ts" />
+﻿/// <reference path="d.ts/lib.d.ts"/>
+/// <reference path="d.ts/lib.es6.d.ts"/>
+/// <reference path="d.ts/jquery.d.ts" />
 /// <reference path="d.ts/babylon.d.ts" />
-/// <reference path="d.ts/lib.d.ts"/>
