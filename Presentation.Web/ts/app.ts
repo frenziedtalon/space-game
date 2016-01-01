576064092b3dd2a644574e6b00448bb617de0f93
Pete Jespers <petejespers@me.com>
2016-01-01 07:49:04 +0000

    Changed all comments to begin with lowercase letter

--- Presentation.Web/ts/app.ts
+++ Presentation.Web/ts/app.ts
@@ -3,42 +3,41 @@
 
 var createScene = () => {
 
-    // Get the canvas element from our HTML below
     var canvas = document.getElementById("renderCanvas");
+    // get the canvas element from our HTML below
 
-    // Load the BABYLON 3D engine
+    // load the BABYLON 3D engine
     var engine = new BABYLON.Engine(canvas, true);
 
-    // Create a scene
+    // create a scene
     var scene = new BABYLON.Scene(engine);
-    scene.clearColor = new BABYLON.Color3(0, 0, 0); // Set background to black
+    scene.clearColor = new BABYLON.Color3(0, 0, 0); // set background to black
 
-    // Create a camera
+    // create a camera
     createCamera();
 
-    // Create a skybox
+    // create a skybox
     createSkybox();
 
-    // Retrieve the objects to be rendered in the scene
+    // retrieve the objects to be rendered in the scene
     retrieveSceneObjects();
 
-
-    // Watch for browser/canvas resize events
+    // watch for browser/canvas resize events
     window.addEventListener("resize", function () {
         engine.resize();
     });
 
-    // Listen for click events
+    // listen for click events
     window.addEventListener("click", function (evt) {
-        // See if there's a mesh under the click
+        // see if there's a mesh under the click
         var pickResult = scene.pick(evt.clientX, evt.clientY);
-        // If there is a hit and we can select the object then set it as the camera target
+        // if there is a hit and we can select the object then set it as the camera target
         if (pickResult.hit) {
             scene.activeCamera.parent = pickResult.pickedMesh;
         }
     });
 
-    // Listen for key presses
+    // listen for key presses
     window.addEventListener("keypress", function (evt) {
         if (evt.keyCode === 32) {
             // spacebar
@@ -53,15 +52,15 @@ var createScene = () => {
     function createSkybox() {
         var skybox = BABYLON.Mesh.CreateBox("Skybox", 5000, scene);
         var skyboxMaterial = new BABYLON.StandardMaterial("skyboxMaterial", scene);
-        skyboxMaterial.backFaceCulling = false; // Render the inside of the skybox
+        skyboxMaterial.backFaceCulling = false; // render the inside of the skybox
         skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
         skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
 
-        // Add textures to the skybox
+        // add textures to the skybox
         skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("Assets/Images/Skybox/skybox", scene);
         skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
 
-        skybox.infiniteDistance = true; // Have the skybox move with the camera so we can never move outside it
+        skybox.infiniteDistance = true; // have the skybox move with the camera so we can never move outside it
         skybox.material = skyboxMaterial;
         skybox.isPickable = false;
     }
@@ -146,13 +145,13 @@ var createScene = () => {
 
     function renderStar(starInfo) {
 
-        // Create a star
+        // create a star
         var starPosition = createPosition(starInfo.Orbit.Position);
 
         var star = BABYLON.Mesh.CreateSphere(starInfo.Name, 16, starInfo.Radius * 2, scene);
         star.position = starPosition;
 
-        // Create the material for the star, removing its reaction to other light sources
+        // create the material for the star, removing its reaction to other light sources
         var starMaterial = new BABYLON.StandardMaterial(starInfo.Name + "Material", scene);
         starMaterial.emissiveTexture = new BABYLON.Texture("Assets/Images/Star/" + starInfo.Texture, scene);
         starMaterial.specularColor = zeroColor();
@@ -163,7 +162,7 @@ var createScene = () => {
         star.info = starInfo;
         star.isPickable = starInfo.CameraTarget;
 
-        // Create a light to make the star shine
+        // create a light to make the star shine
         var starLight = new BABYLON.PointLight(starInfo.Name + "Light", starPosition, scene);
         starLight.intensity = 2;
         starLight.range = 380;
@@ -176,7 +175,7 @@ var createScene = () => {
         planet.info = planetInfo;
         planet.position = createPosition(planetInfo.Orbit.Position);
 
-        // Create a material for the planet
+        // create a material for the planet
         var planetMaterial = new BABYLON.StandardMaterial(planetInfo.Name + "Material", scene);
         planetMaterial.diffuseTexture = new BABYLON.Texture("Assets/Images/Planet/" + planetInfo.Texture, scene);
         planetMaterial.specularColor = zeroColor();
@@ -184,10 +183,10 @@ var createScene = () => {
 
         planet.isPickable = planetInfo.CameraTarget;
 
-        // Draw planet's orbit
+        // draw planet's orbit
         drawCircle(planetInfo.Orbit.Radius, planetInfo.Name + "Orbit");
 
-        // Create any moons
+        // create any moons
         if (planetInfo.hasOwnProperty("Moons")) {
             for (var j = 0; j < planetInfo.Moons.length; j++) {
                 renderMoon(planetInfo.Moons[j], planet);
@@ -199,7 +198,7 @@ var createScene = () => {
         var moon = BABYLON.Mesh.CreateSphere(moonInfo.Name, 16, moonInfo.Radius * 2, scene);
 
         if (parent !== undefined) {
-            // Positions applied are in addition to those of the parent
+            // positions applied are in addition to those of the parent
             moon.parent = parent;
         }
         moon.info = moonInfo;
@@ -208,13 +207,13 @@ var createScene = () => {
 
         moon.position = createPosition(moonInfo.Orbit.Position);
 
-        // Create a material for the moon
+        // create a material for the moon
         var moonMaterial = new BABYLON.StandardMaterial(moonInfo.Name + "Material", scene);
         moonMaterial.diffuseTexture = new BABYLON.Texture("Assets/Images/Moon/" + moonInfo.Texture, scene);
         moonMaterial.specularColor = zeroColor();
         moon.material = moonMaterial;
 
-        // Draw moon's orbit
+        // draw moon's orbit
         drawCircle(moonInfo.Orbit.Radius, moonInfo.Name + "Orbit", parent);
 
     }
@@ -229,7 +228,7 @@ var createScene = () => {
             }
         }
 
-        // Register a render loop to repeatedly render the scene
+        // register a render loop to repeatedly render the scene
         engine.runRenderLoop(function () {
             scene.render();
         });
@@ -239,7 +238,7 @@ var createScene = () => {
         animateScene = !animateScene;
     }
 
-    // Used in debugging, will not be required when turn based gameplay is implemented
+    // used in debugging, will not be required when turn based gameplay is implemented
     function animate() {
         var meshes = scene.meshes;
         for (var i = 0; i < meshes.length; i++) {
@@ -278,7 +277,7 @@ var createScene = () => {
         circle.color = new BABYLON.Color3(0.54, 0.54, 0.54);
 
         if (parent !== undefined) {
-            // Positions applied are in addition to those of the parent
+            // positions applied are in addition to those of the parent
             circle.parent = parent;
         }
 
@@ -294,10 +293,10 @@ var createScene = () => {
         camera.lowerRadiusLimit = 50;
         camera.upperRadiusLimit = 400;
 
-        // Use the new camera
+        // use the new camera
         scene.activeCamera = camera;
 
-        // Let the user move the camera
+        // let the user move the camera
         camera.attachControl(canvas, false);
     }
 
@@ -305,10 +304,10 @@ var createScene = () => {
         var camera = new BABYLON.VRDeviceOrientationFreeCamera("VRDOCamera", new BABYLON.Vector3(0, 50, 50), scene);
         camera.setTarget(new BABYLON.Vector3(0, 0, 0));
 
-        // Use the new camera
+        // use the new camera
         scene.activeCamera = camera;
 
-        // Let the user move the camera
+        // let the user move the camera
         camera.attachControl(canvas, false);
 
     }
