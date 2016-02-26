3b9bf4ff2b9e051987881669059e92314409fed0
Pete Jespers <petejespers@me.com>
2016-02-26 07:23:42 +0000

    SG-9 Dependency injection for TurnController

--- WebApi/Controllers/TurnController.vb
+++ WebApi/Controllers/TurnController.vb
@@ -7,11 +7,12 @@ Namespace Controllers
     Public Class TurnController
         Inherits ApiController
 
-        Public Function EndTurn(turnService As ITurnTrackerService) As TurnResult
+        Public Function EndTurn(turnService As ITurnTrackerService,
+                                entityManager As IEntityManager) As TurnResult
 
-            ' TODO: implement IoC container for turnservice using a singleton
+            ' TODO: implement IoC container for turnservice and entityManager using a singleton
             turnService.Update()
-            EntityManager.Instance().UpdateAll()
+            entityManager.UpdateAll()
 
             Return New TurnResult()
         End Function
