abda614b1dcee326f6021f41c680db1d4b498783
Pete Jespers <petejespers@me.com>
2016-03-13 17:36:50 +0000

    SG-9 Asteroid now inherits OrbitingCelestialObjectBase

--- OrbitalMechanics/CelestialObjects/Asteroid.vb
+++ OrbitalMechanics/CelestialObjects/Asteroid.vb
@@ -1,33 +1,19 @@
 ﻿
-Imports System.Windows.Media.Media3D
 Imports Entities
 Imports OrbitalMechanics.Classes
 
 Namespace CelestialObjects
     Public Class Asteroid
-        Inherits BaseCelestialObject
+        Inherits OrbitingCelestialObjectBase
 
         Public Sub New(name As String,
                        mass As Integer,
                        texture As String,
-                       orbit As Orbit,
+                       orbit As IOrbit,
                        entityManager As IEntityManager)
 
-            MyBase.New(name, mass, texture, entityManager)
-            _orbit = orbit
+            MyBase.New(name, mass, texture, orbit, entityManager)
         End Sub
-
-        Public Overrides Sub Update()
-            Throw New NotImplementedException()
-            Orbit.Update()
-        End Sub
-
-        Private ReadOnly _orbit As Orbit
-        Public ReadOnly Property Orbit As Orbit
-            Get
-                Return _orbit
-            End Get
-        End Property
 
     End Class
 End Namespace
