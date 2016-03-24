e16135df0bf80467495876fdd669e592fd993c7f
Pete Jespers <petejespers@me.com>
2016-03-24 15:58:22 +0000

    SG-9 SG-73 Added reasonably accurate orbital periods for the solar system

--- WebApi/Models/SceneConstructor.vb
+++ WebApi/Models/SceneConstructor.vb
@@ -21,9 +21,9 @@ Namespace Models
 
             Dim planets As New List(Of OrbitingCelestialObjectBase)
 
-            'planets.Add(New Planet("Mercury", 1, "mercury.jpg", 2, New Orbit(40, 0.004, 0), entityManager))
+            planets.Add(New Planet("Mercury", 1, "mercury.jpg", 2, New Orbit(40, TimeSpan.FromDays(88.02525), 0, _turnTracker), _entityManager))
 
-            'planets.Add(New Planet("Venus", 1, "venus.jpg", 4, New Orbit(80, 0.0036, 0), entityManager))
+            planets.Add(New Planet("Venus", 1, "venus.jpg", 4, New Orbit(80, TimeSpan.FromDays(224.62875), 0, _turnTracker), _entityManager))
 
             Dim moon = New Moon("Moon", 1, "moon.png", 1, New Orbit(8, TimeSpan.FromDays(28), 0, _turnTracker), _entityManager)
 
@@ -32,17 +32,17 @@ Namespace Models
 
             planets.Add(earth)
 
-            'planets.Add(New Planet("Mars", 1, "mars.jpg", 5, New Orbit(160, 0.003, 0), entityManager))
+            planets.Add(New Planet("Mars", 1, "mars.jpg", 5, New Orbit(160, TimeSpan.FromDays(686.67), 0, _turnTracker), _entityManager))
 
-            'planets.Add(New Planet("Jupiter", 1, "jupiter.jpg", 10, New Orbit(200, 0.0028, 0), entityManager))
+            planets.Add(New Planet("Jupiter", 1, "jupiter.jpg", 10, New Orbit(200, TimeSpan.FromDays(4334.42175), 0, _turnTracker), _entityManager))
 
-            'planets.Add(New Planet("Saturn", 1, "saturn.jpg", 7, New Orbit(240, 0.0025, 0), entityManager))
+            planets.Add(New Planet("Saturn", 1, "saturn.jpg", 7, New Orbit(240, TimeSpan.FromDays(10760.63), 0, _turnTracker), _entityManager))
 
-            'planets.Add(New Planet("Uranus", 1, "uranus.jpg", 3, New Orbit(280, 0.002, 0), entityManager))
+            planets.Add(New Planet("Uranus", 1, "uranus.jpg", 3, New Orbit(280, TimeSpan.FromDays(30691.9575), 0, _turnTracker), _entityManager))
 
-            'planets.Add(New Planet("Neptune", 1, "neptune.jpg", 5, New Orbit(320, 0.0015, 0), entityManager))
+            planets.Add(New Planet("Neptune", 1, "neptune.jpg", 5, New Orbit(320, TimeSpan.FromDays(60198.67875), 0, _turnTracker), _entityManager))
 
-            'planets.Add(New Planet("Pluto", 1, "pluto.jpg", 4, New Orbit(360, 0.001, 0), entityManager))
+            planets.Add(New Planet("Pluto", 1, "pluto.jpg", 4, New Orbit(360, TimeSpan.FromDays(90602.81925), 0, _turnTracker), _entityManager))
 
             Dim sun = New Star("Sol", 1, 5500, "sun.jpg", 20, Nothing, _entityManager)
             sun.AddSatellite(planets)
