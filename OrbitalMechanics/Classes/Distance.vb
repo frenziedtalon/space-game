1a0655e915e4e51a609eac5e85bc997e88e5e90c
Pete Jespers <petejespers@me.com>
2016-04-07 17:19:10 +0100

    SG-5 Added a Distance class to aid in coverting to/from astronomical units

---
+++ OrbitalMechanics/Classes/Distance.vb
@@ -0,0 +1,35 @@
+﻿Namespace Classes
+    Public Class Distance
+        Private ReadOnly _kilometers As Double
+
+        Const KilometersInAstronomicalUnit As Double = 149597870.7
+
+        Private Sub New(kilometers As Double)
+            If kilometers < 0 Then
+                Throw New ArgumentOutOfRangeException(NameOf(kilometers), "Distance should be greater than zero.")
+            End If
+            _kilometers = kilometers
+        End Sub
+
+        Public ReadOnly Property AstronomicalUnits As Double
+            Get
+                Return _kilometers * KilometersInAstronomicalUnit
+            End Get
+        End Property
+
+        Public Shared Function FromAstronomicalUnits(astronomicalUnits As Double) As Distance
+            Return New Distance(astronomicalUnits * KilometersInAstronomicalUnit)
+        End Function
+
+        Public ReadOnly Property Kilometers As Double
+            Get
+                Return _kilometers
+            End Get
+        End Property
+
+        Public Shared Function FromKilometers(kilometers As Double) As Distance
+            Return New Distance(kilometers)
+        End Function
+
+    End Class
+End Namespace
