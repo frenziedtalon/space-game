38c6eff99fcc3447488b853c628b9de43d84c90d
Pete Jespers <petejespers@me.com>
2016-04-07 17:15:16 +0100

    SG-5 Added Angle class

---
+++ Core/Classes/Angle.vb
@@ -0,0 +1,23 @@
+﻿Namespace Classes
+    Public Class Angle
+        Private ReadOnly _radians As Double
+
+        Private Sub New(radians As Double)
+            If radians < 0 OrElse radians > 2 * Math.PI Then
+                Throw New ArgumentOutOfRangeException(NameOf(radians), "Angle should be between 0 and 2*Math.Pi")
+            End If
+            _radians = radians
+        End Sub
+
+        Public ReadOnly Property Radians As Double
+            Get
+                Return _radians
+            End Get
+        End Property
+
+        Public Shared Function FromRadians(angle As Double) As Angle
+            Return New Angle(angle)
+        End Function
+
+    End Class
+End Namespace
