c17f9ea074e1b2a45ed8abcaf102f3fc46b42a52
Pete Jespers <petejespers@me.com>
2016-04-25 20:45:59 +0100

    SG-5 Added a Constants class

---
+++ OrbitalMechanics/Constants.vb
@@ -0,0 +1,5 @@
+﻿Public Class Constants
+    Public Const DaysInJulianYear = 365.25
+    Public Const MonthsInJulianYear = 12
+    Public Const DaysInJulianMonth = DaysInJulianYear / MonthsInJulianYear
+End Class
