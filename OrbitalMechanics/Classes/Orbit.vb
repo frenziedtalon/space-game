33f18205a1dfbb7a5ad12e0965dc73bcc336d9f8
Pete Jespers <petejespers@me.com>
2016-05-17 16:39:18 +0100

    SG-5 Beginnings of changes to IOrbit and Orbit. Will not compile.

--- OrbitalMechanics/Classes/Orbit.vb
+++ OrbitalMechanics/Classes/Orbit.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Windows.Media.Media3D
+Imports Core.Classes
 Imports TurnTracker
 
 Namespace Classes
@@ -8,87 +9,210 @@ Namespace Classes
 
         Private ReadOnly _turnTracker As ITurnTracker
 
-        Public Sub New(radius As Integer,
-                       period As TimeSpan,
-                       angle As Double,
-                       turnTracker As ITurnTracker)
-            Me.New(radius, period, angle, New Point3D(0, 0, 0), turnTracker)
-            SetInitialPosition()
-        End Sub
-
-        Private Sub New(radius As Integer,
-                       period As TimeSpan,
-                       angle As Double,
-                       position As Point3D,
-                       turnTracker As ITurnTracker)
-            _radius = radius
-            _period = period
-            _angle = angle
-            _position = position
+        Public Sub New(turnTracker As ITurnTracker,
+                        semiMajorAxis As Distance,
+                        eccentricity As Double,
+                        inclination As Angle,
+                        argumentOfPeriapsis As Angle,
+                        longitudeOfAscendingNode As Angle,
+                        meanAnomalyZero As Angle)
             _turnTracker = turnTracker
+            _semiMajorAxis = semiMajorAxis
+            _eccentricity = eccentricity
+            _inclination = inclination
+            _argumentOfPeriapsis = argumentOfPeriapsis
+            _longitudeOfAscendingNode = longitudeOfAscendingNode
+            _meanAnomalyZero = meanAnomalyZero
         End Sub
 
-        Private ReadOnly _radius As Integer
-        Public ReadOnly Property Radius As Integer Implements IOrbit.Radius
-            Get
-                Return _radius
-            End Get
-        End Property
-
-        Private _speed As Double = 0
-        Public ReadOnly Property Speed As Double Implements IOrbit.Speed
-            Get
-                If Double.Equals(_speed, 0.0) AndAlso _turnTracker IsNot Nothing Then
-                    _speed = (2 * Math.PI) / TurnsPerOrbit()
-                End If
-                Return _speed
-            End Get
-        End Property
-
-        Private ReadOnly Property _period As TimeSpan
+        Private _period As TimeSpan = TimeSpan.Zero
         Public ReadOnly Property Period As TimeSpan Implements IOrbit.Period
             Get
+                If _period = TimeSpan.Zero AndAlso SemiMajorAxis IsNot Nothing Then
+                    Dim years = Math.Pow(SemiMajorAxis.AstronomicalUnits, 1.5)
+                    _period = TimeSpan.FromDays(years * Constants.DaysInJulianYear)
+                End If
                 Return _period
             End Get
         End Property
 
-        Private _angle As Double
-        Public ReadOnly Property Angle As Double Implements IOrbit.Angle
+        Public ReadOnly Property Position As Point3D Implements IOrbit.Position
             Get
-                Return _angle
+                _eccentricAnomaly = CalculateEccentricAnomaly()
+                Return New Point3D(CalculateX(), CalculateY(), CalculateZ())
             End Get
         End Property
 
-        Private _position As Point3D
+        Private Function TrueAnomaly() As Angle Implements IOrbit.TrueAnomaly
+            Throw New NotImplementedException()
+            ' uses _eccentricAnomaly
+            ' cosn(t) = (cosE(t)-e) / (1 - ecosE(t))
+            ' n(t) = true anomaly
+            ' E(t) = eccentric anomaly
+            ' e = eccentricity
+        End Function
 
-        Public ReadOnly Property Position As Point3D Implements IOrbit.Position
+        Private ReadOnly _longitudeOfAscendingNode As Angle
+        Public ReadOnly Property LongitudeOfAscendingNode As Angle Implements IOrbit.LongitudeOfAscendingNode
             Get
-                Return _position
+                Return _longitudeOfAscendingNode
+            End Get
+        End Property
+
+        Private ReadOnly _inclination As Angle
+        Public ReadOnly Property Inclination As Angle Implements IOrbit.Inclination
+            Get
+                Return _inclination
+            End Get
+        End Property
+
+        Private ReadOnly _argumentOfPeriapsis As Angle
+        Public ReadOnly Property ArgumentOfPeriapsis As Angle Implements IOrbit.ArgumentOfPeriapsis
+            Get
+                Return _argumentOfPeriapsis
+            End Get
+        End Property
+
+        Private ReadOnly _semiMajorAxis As Distance
+        ''' <summary>
+        ''' One half of the major axis, represents the mean distance from the primary 
+        ''' </summary>
+        Public ReadOnly Property SemiMajorAxis As Distance Implements IOrbit.SemiMajorAxis
+            Get
+                Return _semiMajorAxis
+            End Get
+        End Property
+
+        Private ReadOnly _eccentricity As Double
+        Public ReadOnly Property Eccentricity As Double Implements IOrbit.Eccentricity
+            Get
+                Return _eccentricity
+            End Get
+        End Property
+
+        Private Function MeanAnomaly() As Angle Implements IOrbit.MeanAnomaly
+
+            Dim radians = MeanAnomalyZero.Radians + (MeanAngularMotion() * _turnTracker.TimeSinceStart.TotalDays)
+
+            Return Angle.FromRadians(radians)
+        End Function
+
+        Private _periapsisDistance As Distance
+        Public ReadOnly Property PeriapsisDistance As Distance Implements IOrbit.PeriapsisDistance
+            Get
+                If _periapsisDistance Is Nothing Then
+                    _periapsisDistance = Distance.FromAstronomicalUnits(SemiMajorAxis.AstronomicalUnits * (1 - Eccentricity))
+                End If
+                Return _periapsisDistance
+            End Get
+        End Property
+
+        Private _apapsisDistance As Distance
+        Public ReadOnly Property ApapsisDistance As Distance Implements IOrbit.ApapsisDistance
+            Get
+                If _apapsisDistance Is Nothing Then
+                    _apapsisDistance = Distance.FromAstronomicalUnits(SemiMajorAxis.AstronomicalUnits * (1 + Eccentricity))
+                End If
+                Return _apapsisDistance
+            End Get
+        End Property
+
+        Private ReadOnly _meanAnomalyZero As Angle
+        Public ReadOnly Property MeanAnomalyZero() As Angle Implements IOrbit.MeanAnomalyZero
+            Get
+                Return _meanAnomalyZero
             End Get
         End Property
 
         Public Sub Update() Implements IOrbit.Update
-            ' update the Position based on the orbit
-            ' currently a circular orbit
-
-            If Speed > 0.0 Then
-                Dim x = Radius * Math.Sin(Angle)
-                Dim y = 0
-                Dim z = Radius * Math.Cos(Angle)
-
-                _position = New Point3D(x, y, z)
-
-                _angle += Speed
-            End If
-        End Sub
-
-        Private Sub SetInitialPosition()
-            ' TODO: Calculate properly including the angle
-            _position = New Point3D(0, 0, _radius)
+            ' position is calculated on request
         End Sub
 
         Private Function TurnsPerOrbit() As Double
             Return Period.Ticks / _turnTracker.TurnLength.Ticks
+        End Function
+
+        Private Function Range() As Distance
+            Dim aus = (SemiMajorAxis.AstronomicalUnits * (1 - (Math.Pow(Eccentricity, 2)))) / (1 + (Eccentricity * (Math.Cos(TrueAnomaly.Radians))))
+            Return Distance.FromAstronomicalUnits(aus)
+        End Function
+
+        'R,X,Y,Z-Heliocentric Distances
+        'TA - True Anomaly
+        'N - Longitude of the Ascending Node
+        'w - Argument of the Perihelion
+        'i - inclination
+
+        'X = R * (Cos(N) * Cos(TA + w) - Sin(N) * Sin(TA+w)*Cos(i)
+        Private Function CalculateX() As Double
+            Return Range().Kilometers * ((Math.Cos(LongitudeOfAscendingNode.Radians) * Math.Cos(TrueAnomaly.Radians + ArgumentOfPeriapsis.Radians)) - (Math.Sin(LongitudeOfAscendingNode.Radians) * Math.Sin(TrueAnomaly.Radians + ArgumentOfPeriapsis.Radians))) * Math.Cos(Inclination.Radians)
+        End Function
+
+        'Y = R * (Sin(N) * Cos(TA+w) + Cos(N) * Sin(TA+w)) * Cos(i))
+        Private Function CalculateY() As Double
+            Return Range().Kilometers * ((Math.Sin(LongitudeOfAscendingNode.Radians) * Math.Cos(TrueAnomaly.Radians + ArgumentOfPeriapsis.Radians)) + (Math.Cos(LongitudeOfAscendingNode.Radians)) * Math.Sin(TrueAnomaly.Radians + ArgumentOfPeriapsis.Radians)) * Math.Cos(Inclination.Radians)
+        End Function
+
+        'Z = R * Sin(TA+w) * Sin(i)
+        Private Function CalculateZ() As Double
+            Return Range().Kilometers * Math.Sin(TrueAnomaly.Radians + ArgumentOfPeriapsis.Radians)
+        End Function
+
+        Private Function LongitudeOfPeriapsis() As Angle
+            Return Angle.FromRadians(LongitudeOfAscendingNode.Radians + ArgumentOfPeriapsis.Radians)
+        End Function
+
+        Private Function MeanLongitude() As Angle
+            Return Angle.FromRadians(MeanAnomaly.Radians + LongitudeOfAscendingNode.Radians)
+        End Function
+
+        ''' <summary>
+        ''' Radians moved in the orbit per day
+        ''' </summary>
+        Private Function MeanAngularMotion() As Double
+            Return (2 * Math.PI) / Period.TotalDays
+        End Function
+
+        Private _eccentricAnomaly As Angle
+
+        Private Function CalculateEccentricAnomaly() As Angle
+
+            Dim threshold = Angle.FromDegrees(0.001).Radians
+            Dim maxIterations = 50
+
+            Dim iterations = 0
+
+            ' E = M + e * sin(M) * ( 1.0 + e * cos(M) )
+            ' E = M + (e * sin(M) * ( 1.0 + (e * cos(M)) ))
+            'Dim E0 = MeanAnomaly().Radians + (
+            '    Eccentricity * Math.Sin(MeanAnomaly().Radians) *
+            '    (1 + (Eccentricity * Math.Cos(MeanAnomaly().Radians)))
+            ')
+
+            ' initial guess
+            Dim E0 As Double = If(Eccentricity > 0.8, Math.PI, MeanAnomaly().Radians)
+
+            Do Until iterations >= maxIterations
+                ' E1 = E0 - ( E0 - e * sin(E0) - M ) / ( 1 - e * cos(E0) )
+                Dim E1 As Double = 0.0
+
+
+                If E1 - E0 < threshold Then
+                    Return Angle.FromRadians(E1)
+                End If
+
+                iterations += 1
+
+            Loop
+
+            ' values are not converging, eccentricity probably near to 1. Use calculation for a near-parabolic or parabolic orbit.
+
+            If Double.Equals(Eccentricity, 1.0) Then
+                ' http://www.stjarnhimlen.se/comp/ppcomp.html#18
+
+            Else
+                ' http://www.stjarnhimlen.se/comp/ppcomp.html#19
+            End If
         End Function
     End Class
 End Namespace
---
+++ OrbitalMechanics/IOrbit.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Windows.Media.Media3D
+Imports Core.Classes
+Imports OrbitalMechanics.Classes
+
+
+Public Interface IOrbit
+    ReadOnly Property Position As Point3D
+    ReadOnly Property Period As TimeSpan
+    Sub Update()
+    ''' <summary>
+    ''' Angle between planet and perihelion.
+    ''' </summary>
+    ''' <remarks>0 at perihelion. Unit is radians. Changes most rapidly at perihelion.</remarks>
+    Function TrueAnomaly() As Angle
+    ReadOnly Property LongitudeOfAscendingNode As Angle
+    ReadOnly Property Inclination As Angle
+    ReadOnly Property ArgumentOfPeriapsis As Angle
+    ''' <summary>
+    ''' Mean distance to the primary.
+    ''' </summary>
+    ReadOnly Property SemiMajorAxis As Distance
+    ''' <summary>
+    ''' 0=circle, 0-1=ellipse, 1=parabola.
+    ''' </summary>
+    ''' <remarks>Units=AU</remarks>
+    ReadOnly Property Eccentricity As Double
+    ''' <summary>
+    ''' Angle of average orbital motion.
+    ''' </summary>
+    ''' <remarks>0 at periapsis. Increases uniformly with time.</remarks>
+    Function MeanAnomaly() As Angle
+    ''' <summary>
+    ''' Closest distance to the primary.
+    ''' </summary>
+    ''' <remarks>Units=AU</remarks>
+    ReadOnly Property PeriapsisDistance As Distance
+    ''' <summary>
+    ''' Furthest distance from the primary.
+    ''' </summary>
+    ''' <remarks>Units=AU</remarks>
+    ReadOnly Property ApapsisDistance As Distance
+    ''' <summary>
+    ''' Represents the angle at which the object lies within its orbit at zero time
+    ''' </summary>
+    ReadOnly Property MeanAnomalyZero() As Angle
+End Interface
