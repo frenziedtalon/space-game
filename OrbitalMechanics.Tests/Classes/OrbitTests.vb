dd31aaf26e68621de51ac7b92f4cba52193f2b3f
Pete Jespers <petejespers@me.com>
2016-05-17 16:39:40 +0100

    SG-5 Tests for Orbit.Period

--- OrbitalMechanics.Tests/Classes/OrbitTests.vb
+++ OrbitalMechanics.Tests/Classes/OrbitTests.vb
@@ -1,5 +1,4 @@
 ﻿Imports Core.Classes
-Imports Microsoft.VisualBasic.CompilerServices
 Imports NSubstitute
 Imports NUnit.Framework
 Imports OrbitalMechanics.Classes
@@ -10,28 +9,31 @@ Namespace Classes
     <TestFixture>
     Public Class OrbitTests
 
-        <TestCase(0.1, 0.032)>
+        <TestCase(0.1, 0.0316227766)>
         <TestCase(1, 1)>
-        <TestCase(1.245, 1.389)>
-        <TestCase(1.5, 1.837)>
-        <TestCase(6, 14.697)>
+        <TestCase(1.245, 1.389165622)>
+        <TestCase(1.5, 1.837117307)>
+        <TestCase(6, 14.69693846)>
         Public Sub Period_WhenCalled_CalculatesCorrectValue(semiMajorAxis As Double, expectedResultYears As Double)
 
+            Const acceptableDelta = 1 / (1 * 60 * 60 * 24)
             Dim turnTracker As ITurnTracker = Substitute.For(Of ITurnTracker)
+            Dim zeroAngle = Angle.FromRadians(0)
 
             Dim orbit As New Orbit(turnTracker:=turnTracker,
                                    semiMajorAxis:=Distance.FromAstronomicalUnits(semiMajorAxis),
                                    eccentricity:=0,
-                                   inclination:=Angle.FromRadians(0),
-                                   argumentOfPeriapsis:=Angle.FromRadians(0),
-                                   longitudeOfAscendingNode:=Angle.FromRadians(0),
-                                   meanAnomalyZero:=Angle.FromRadians(0))
+                                   inclination:=zeroAngle,
+                                   argumentOfPeriapsis:=zeroAngle,
+                                   longitudeOfAscendingNode:=zeroAngle,
+                                   meanAnomalyZero:=zeroAngle)
+
 
             Dim expectedDays = expectedResultYears * DaysInJulianYear
 
-            Dim resultDays = orbit.Period.Days
+            Dim resultDays = orbit.Period.TotalDays
 
-            Assert.AreEqual(resultDays, expectedDays)
+            Assert.AreEqual(expectedDays, resultDays, acceptableDelta)
 
         End Sub
 
