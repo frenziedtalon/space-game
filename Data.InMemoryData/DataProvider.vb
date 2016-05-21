d0bd31a8c43840f336d1bc45809c50997a919f89
Pete Jespers <petejespers@me.com>
2016-05-21 21:21:12 +0100

    SG-5 Added Data.InMemoryData project along with orbital data for the major objects in the solar system

---
+++ Data.InMemoryData/DataProvider.vb
@@ -0,0 +1,15 @@
+﻿Imports Data
+
+Public Class DataProvider
+    Implements IDataProvider
+
+    Private _solarSystem As SolarSystemData
+    Public ReadOnly Property SolarSystem As ISolarSystemData Implements IDataProvider.SolarSystem
+        Get
+            If _solarSystem Is Nothing Then
+                _solarSystem = New SolarSystemData()
+            End If
+            Return _solarSystem
+        End Get
+    End Property
+End Class
