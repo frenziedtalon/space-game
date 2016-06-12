48a430f8e6b3f2eb908c5ea04c902559c960716d
Pete Jespers <petejespers@me.com>
2016-06-12 20:34:44 +0100

    SG-98 Class to represent Mass

---
+++ Core.Tests/Classes/MassTests.vb
@@ -0,0 +1,34 @@
+﻿Imports Core.Classes
+Imports NUnit.Framework
+
+Namespace Classes
+    <TestFixture>
+    Public Class MassTests
+
+        <TestCase(0.0)>
+        <TestCase(56.0)>
+        <TestCase(0.00000001)>
+        <TestCase(25444588655.0)>
+        Public Sub FromKilograms_WhenCalled_StoresCorrectValue(kilograms As Double)
+
+            Dim m = Mass.FromKilograms(kilograms)
+
+            Dim result = m.Kilograms
+
+            Assert.AreEqual(kilograms, result)
+        End Sub
+
+        <TestCase(0.0)>
+        <TestCase(56.0)>
+        <TestCase(0.00000001)>
+        <TestCase(25444588655.0)>
+        Public Sub FromSolarMasses_WhenCalled_StoresCorrectValue(solarMasses As Double)
+            Dim acceptableDelta = Mass.FromKilograms(1).Kilograms
+            Dim m = Mass.FromSolarMasses(solarMasses)
+
+            Dim result = m.SolarMasses
+
+            Assert.AreEqual(solarMasses, result, acceptableDelta)
+        End Sub
+    End Class
+End Namespace
