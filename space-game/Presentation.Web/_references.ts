d6e74f6b3f3a6f160cb79a7d63488d749acb62c9
Pete Jespers <petejespers@me.com>
2016-07-26 20:52:54 +0100

    SG-111 Updated to babylonjs version 2.4

--- space-game/Presentation.Web/_references.ts
+++ space-game/Presentation.Web/_references.ts
@@ -1,5 +1,5 @@
 ﻿/// <reference path="ts/d.ts/jquery.d.ts" />
-/// <reference path="ts/d.ts/babylon.d.ts" />
+/// <reference path="ts/d.ts/babylon-2.4.d.ts" />
 /// <reference path="ts/app.ts"/>
 /// <reference path="ts/Classes/ICelestialObject.ts"/>
 /// <reference path="ts/Classes/BaseGameEntity.ts"/>
