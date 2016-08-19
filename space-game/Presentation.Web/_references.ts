3dd5682a3add069ab2c3050e76f6afacc0d3b9df
Pete Jespers <petejespers@me.com>
2016-08-19 20:50:44 +0100

    Reshuffle of Typescript files

--- space-game/Presentation.Web/_references.ts
+++ space-game/Presentation.Web/_references.ts
@@ -1,7 +1,7 @@
 ﻿/// <reference path="ts/d.ts/jquery.d.ts" />
 /// <reference path="ts/d.ts/babylon-2.4.d.ts" />
 /// <reference path="ts/app.ts"/>
-/// <reference path="ts/Classes/ICelestialObject.ts"/>
-/// <reference path="ts/Classes/BaseGameEntity.ts"/>
-/// <reference path="ts/Classes/BaseCelestialObject.ts"/>
-/// <reference path="ts/Classes/OrbitingCelestialObjectBase.ts"/>
+/// <reference path="ts/Interfaces/ICelestialObject.ts"/>
+/// <reference path="ts/Classes/Base/BaseGameEntity.ts"/>
+/// <reference path="ts/Classes/Base/BaseCelestialObject.ts"/>
+/// <reference path="ts/Classes/Base/OrbitingCelestialObjectBase.ts"/>
