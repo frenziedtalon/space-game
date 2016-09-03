ea15fd312041ef02d6a0a720eefcbfaa7f861fac
Pete Jespers <petejespers@me.com>
2016-09-03 07:07:45 +0100

    SG-135 Updated sql data model

--- space-game/Data.SqlDataProvider/Model/SolarSystemEntities.Designer.vb
+++ space-game/Data.SqlDataProvider/Model/SolarSystemEntities.Designer.vb
@@ -1,4 +1,4 @@
-﻿' T4 code generation is enabled for model 'E:\Repos\space-game\Data.SqlDataProvider\Model\SolarSystemEntities.edmx'. 
+﻿' T4 code generation is enabled for model 'E:\Repos\space-game\space-game\Data.SqlDataProvider\Model\SolarSystemEntities.edmx'. 
 ' To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 ' property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 ' is open in the designer.
