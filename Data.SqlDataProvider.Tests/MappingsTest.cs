308d6ce80e2ddee54056916d52af8771a089a92f
Pete Jespers <petejespers@me.com>
2016-09-05 21:42:21 +0100

    SG-135  Added Mapster and mapping for TextureGroup to Textures

---
+++ Data.SqlDataProvider.Tests/MappingsTest.cs
@@ -0,0 +1,65 @@
+﻿using Core.Classes;
+using Mapster;
+using NUnit.Framework;
+
+namespace Data.SqlDataProvider.Tests
+{
+    [TestFixture]
+    class MappingsTest
+    {
+        private TypeAdapterConfig _config;
+
+        [OneTimeSetUp]
+        public void SetupMappings()
+        {
+            var mappings = new Mappings();
+            mappings.Register(TypeAdapterConfig.GlobalSettings);
+            TypeAdapterConfig.GlobalSettings.RequireExplicitMapping = true;
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            TypeAdapterConfig.GlobalSettings.RequireExplicitMapping = false;
+            TypeAdapterConfig.GlobalSettings.Rules.Clear();
+            TypeAdapterConfig.GlobalSettings.RuleMap.Clear();
+        }
+
+
+        [Test]
+        public void TextureGroup_MapTo_Textures()
+        {
+            TextureGroup source = new TextureGroup { Id = 1, Name = "Group" };
+
+            TexturePath lowPath = new TexturePath() {Path = "low/path/"};
+            TexturePath mediumPath = new TexturePath() { Path = "medium/path/" };
+            TexturePath highPath = new TexturePath() { Path = "high/path" };
+
+            TextureType lowTextureType = new TextureType() { Type = "Low" };
+            TextureType mediumTextureType = new TextureType() { Type = "Medium" };
+            TextureType highTextureType = new TextureType() { Type = "High" };
+
+            Texture lowTexture = new Texture() {Name = "/Low.jpg", TexturePath = lowPath, TextureType = lowTextureType};
+            Texture mediumTexture = new Texture() { Name = "Medium.jpg", TexturePath = mediumPath , TextureType = mediumTextureType};
+            Texture highTexture = new Texture() {Name = "/High.jpg", TexturePath = highPath, TextureType = highTextureType};
+            
+            TextureGroupToTexture lowTextureGroupToTexture = new TextureGroupToTexture() {Texture = lowTexture, TextureGroup = source};
+            TextureGroupToTexture mediumTextureGroupToTexture = new TextureGroupToTexture() { Texture = mediumTexture, TextureGroup = source };
+            TextureGroupToTexture highTextureGroupToTexture = new TextureGroupToTexture() { Texture = highTexture, TextureGroup = source };
+
+
+            source.TextureGroupToTextures.Add(lowTextureGroupToTexture);
+            source.TextureGroupToTextures.Add(mediumTextureGroupToTexture);
+            source.TextureGroupToTextures.Add(highTextureGroupToTexture);
+            
+            Textures expected = new Textures { Low = "low/path/Low.jpg", Medium = "medium/path/Medium.jpg", High = "high/path/High.jpg" };
+
+            Textures result = source.Adapt<Textures>();
+
+            Assert.AreEqual(expected.Low, result.Low);
+            Assert.AreEqual(expected.Medium, result.Medium);
+            Assert.AreEqual(expected.High, result.High);
+        }
+
+    }
+}
---
+++ space-game/Data.SqlDataProvider/Mappings.vb
@@ -0,0 +1,28 @@
+﻿Imports Core.Classes
+Imports Core.Helpers
+Imports Mapster
+
+Public Class Mappings
+    Implements IRegister
+
+    Public Sub Register(config As TypeAdapterConfig) Implements IRegister.Register
+
+        config.ForType(Of TextureGroup, Textures).
+            Map(Function(dest) dest.Low, Function(src) CreateTexturePath("Low", src.TextureGroupToTextures)).
+            Map(Function(dest) dest.Medium, Function(src) CreateTexturePath("Medium", src.TextureGroupToTextures)).
+            Map(Function(dest) dest.High, Function(src) CreateTexturePath("High", src.TextureGroupToTextures))
+
+    End Sub
+
+    Private Function CreateTexturePath(typeName As String, group As ICollection(Of TextureGroupToTexture)) As String
+
+        Dim result As String = String.Empty
+        Dim target = group.FirstOrDefault(Function(t) t.Texture.TextureType.Type.Equals(typeName, StringComparison.OrdinalIgnoreCase))
+
+        If target IsNot Nothing Then
+            result = PathHelper.SitePathCombine(target.Texture.TexturePath.Path, target.Texture.Name)
+        End If
+        Return result
+    End Function
+
+End Class
