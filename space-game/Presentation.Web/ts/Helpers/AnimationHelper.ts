716a906cfebe656c77ab49ad36028c76bfefd674
Pete Jespers <petejespers@me.com>
2016-10-02 19:43:16 +0100

    SG-137 Helper to remove all animations

---
+++ space-game/Presentation.Web/ts/Helpers/AnimationHelper.ts
@@ -0,0 +1,8 @@
+﻿"use strict";
+class AnimationHelper {
+    static removeAllAnimations(node: BABYLON.Node) {
+        if (node) {
+            node.animations.splice(0, node.animations.length - 1);
+        }
+    }
+}
