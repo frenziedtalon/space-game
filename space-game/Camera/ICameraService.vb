4183675e369f6f3bb7c2d0768cebd7741bdbb732
Pete Jespers <petejespers@me.com>
2017-02-19 16:35:41 +0000

    #91 Converted Camera project to C#

--- space-game/Camera/ICameraService.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Interface ICameraService
-    ReadOnly Property CurrentTarget As Guid
-    Sub SetTarget(target As String)
-    Sub SetTarget(target As Guid)
-    ReadOnly Property LastTarget As Guid
-End Interface
