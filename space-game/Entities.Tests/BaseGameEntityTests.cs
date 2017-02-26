84f2f4ebf8a4ffdc15c020a5b9bdd4da1c04f4b3
Pete Jespers <petejespers@me.com>
2017-02-26 07:00:17 +0000

    #91 Converted Entities.Tests to C#

---
+++ space-game/Entities.Tests/BaseGameEntityTests.cs
@@ -0,0 +1,21 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+
+namespace Entities.Tests
+{
+    [TestFixture]
+    public class BaseGameEntityTests
+    {
+        [Test]
+        public void ctor_WhenArgumentIsNull_ThrowsArgumentNullException()
+        {
+            IEntityManager entityManager = null;
+            Type expectedType = typeof(ArgumentNullException);
+
+            Exception ex = Assert.Catch<Exception>(() => Substitute.For<BaseGameEntity>(entityManager));
+
+            Assert.AreEqual(ex.InnerException.GetType(), expectedType);
+        }
+    }
+}
